37f8336d723f3937ddd8a6489911b13df125ff61
Emre KURUM <emrekurum07@hotmail.com>
2025-04-29 20:21:42 +0300

    Update firebaseConfig.ts

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -5,13 +5,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore'; // <<< bunu ekledik!
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKz2zily4K26rS9-DlKEzjxIAKmFdlOOg",
-  authDomain: "saglik-takip-56672.firebaseapp.com",
-  projectId: "saglik-takip-56672",
-  storageBucket: "saglik-takip-56672.appspot.com",
-  messagingSenderId: "644480381189",
-  appId: "1:644480381189:web:8730dd5062277ae010c76c",
-  measurementId: "G-LC4ZPBH34Q"
+ 
 };
 
 if (!firebase.apps.length) {
